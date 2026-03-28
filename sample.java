504258e7e7e83b3283428618a7b414cd013950e2
kairashiva12 <105987502+kairashiva12@users.noreply.github.com>
2026-03-28 14:01:00 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,7 +5,7 @@ class Demo{
 	     System.out.println("Welcome to devops world ");
 			     System.out.println("updated code ");
 		for(int i=0;i<2;i++){
-             System.out.println(i+"started....")
+             System.out.println(i+"started....");
 		}
 		
 
