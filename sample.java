0d6ab9416448bd7b9120b8510d9073bce5a2c3b9
kairashiva12 <105987502+kairashiva12@users.noreply.github.com>
2026-03-28 13:11:34 +0530

    Update code2

--- sample.java
+++ sample.java
@@ -3,6 +3,8 @@ class Demo{
 	public static void main(String[] args){
              System.out.println("hello world");
 	     System.out.println("Welcome to devops world ");
+			     System.out.println("updated code ");
+
 	}
 }
 
